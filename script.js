bb2abf511eaa7a4197b86f566ab57d209888c6b4
Avi Desmond <avidesmondlearns@gmail.com>
2022-06-07 09:57:37 +1000

    Add computer logic to determine rock paper or scissors

---
+++ script.js
@@ -0,0 +1,24 @@
+// Create computer logic. Computer needs to generate random number between 0 and 3 and return either Rock, Paper or Scissors.
+function getRockPaperScissors() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return 'scissors'
+    } else if (randomNumber === 1) {
+        return 'rock'
+    } else if (randomNumber === 2) {
+        return 'paper'
+    }
+
+}
+
+
+// Get user inputs. User must be prompted for Rock, Paper or Scissors 
+
+
+// Compare user input with computer input to determine winner. 
+
+
+
+// Check for Winner
+
+// Output Winner 
