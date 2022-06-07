a32e84eb9d7cfeca6e4eff9e6682d55715b4f009
Avi Desmond <avidesmondlearns@gmail.com>
2022-06-07 11:05:23 +1000

    Add function to output winner to the screen and function to initialise game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Create computer logic. Computer needs to generate random number between 0 and 3 and return either Rock, Paper or Scissors.
-function getRockPaperScissors() {
+// Create computer logic. Computer needs to generate random number between 0 and 2 and return either Rock, Paper or Scissors.
+function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         return 'scissors'
@@ -14,13 +14,14 @@ function getRockPaperScissors() {
 
 // Get user inputs. User must be prompted for Rock, Paper or Scissors 
 function getUserInput() {
-    const userChoice = prompt('Rock, Paper Scissors?',).toLowerCase();
-    return userChoice;
+    const userChoice = prompt('Rock, Paper or Scissors?') || ''; // Account for cases where result is null such as when escape key is pressed.
+    const userChoiceLowerCase = userChoice.toLowerCase(); // Convert to lower case to ensure standardised user input
+    return userChoiceLowerCase;
 }
 
 // Compare user input with computer input to determine winner. Needs to get User Choice and compare to Computers choice.
 function getWinner(userChoice, computerChoice) {
-    if (userChoice != 'rock' || userChoice != 'paper' || userChoice != 'scissors') {
+    if (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
         return 'Please enter Rock, Paper or Scissors'
     } else if (userChoice === computerChoice) {
         return 'Tie!'
@@ -31,10 +32,20 @@ function getWinner(userChoice, computerChoice) {
     } else if (userChoice === 'scissors' && computerChoice === 'paper') {
         return `Computer Chose: ${computerChoice}. You Chose: ${userChoice}, you win!`
     } else {
-        return `Computer Chose: ${computerChoice}. You Chose: ${userChoice}, you win!`
+        return `Computer Chose: ${computerChoice}. You Chose: ${userChoice}, you lose :(`
     }
 }
 
 
 
 // Output Winner 
+function outputWinner() {
+    alert(getWinner(getUserInput(), getComputerChoice()));
+}
+
+// Run the Game
+function runGame() {
+    outputWinner();
+}
+
+runGame();
